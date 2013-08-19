4fbd6398769458b81b51a3e01780a8d9e4fb077b
Thalassicus <Thalassicus@hotmail.com>
2013-08-18 19:09:37 -0500

    v3.4 - minor bugfixes

--- Cep/_Project/CEP_Start.sql
+++ Cep/_Project/CEP_Start.sql
@@ -1,3 +1,5 @@
 -- 
 
 INSERT INTO LoadedFile(Type, Value) VALUES ('Cep_Language_End.sql', 0);
+INSERT INTO LoadedFile(Type, Value) VALUES ('CEL__A_to_I.xml', 0);
+INSERT INTO LoadedFile(Type, Value) VALUES ('CEL__J_to_Z.xml', 0);
