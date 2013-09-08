de4ab05675089bd4b7930eb5eede1d0eb5a1808f
Thalassicus <Thalassicus@hotmail.com>
2013-09-08 08:14:47 -0500

    v3.4.2 - bugfixes

--- Cep/CEP_Options.sql
+++ Cep/CEP_Options.sql
@@ -111,19 +111,4 @@ VALUES ('CEP_DEBUG_MODE', 1);
 --
 -- Do not change items below
 
-UPDATE Defines SET Value=1 WHERE Name='QUEST_DISABLED_INVEST' AND EXISTS 
-(SELECT Value FROM Cep WHERE Type='DISABLE_GOLD_GIFTS' AND Value=1);
-
-UPDATE Civilizations SET DawnOfManAudio = "" WHERE EXISTS 
-(SELECT Value FROM Cep WHERE Type='PLAY_SPEECH_START' AND Value=0);
-
-UPDATE Buildings SET WonderSplashAudio = "" WHERE EXISTS 
-(SELECT Value FROM Cep WHERE Type='PLAY_SPEECH_WONDERS' AND Value=0);
-
-UPDATE Technologies SET AudioIntroHeader = "" WHERE EXISTS 
-(SELECT Value FROM Cep WHERE Type='PLAY_SPEECH_TECHS' AND Value=0);
-
-UPDATE Technologies SET AudioIntro = "" WHERE EXISTS 
-(SELECT Value FROM Cep WHERE Type='PLAY_SPEECH_TECHS' AND Value=0);
-
 UPDATE LoadedFile SET Value=1 WHERE Type='Cep_Options.sql';
